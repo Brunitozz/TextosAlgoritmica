a78970c8fe03ef951cf6ff704e1e30bbe9a34048
BRUNOPUMA <122190257+Brunitozz@users.noreply.github.com>
2023-10-06 11:50:09 -0500

    agregando Main

--- Tarea/src/ProjectSupplierPart/Main.java
+++ Tarea/src/ProjectSupplierPart/Main.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ProjectSupplierPart;
-
+import java.util.Scanner;
 /**
  *
  * @author user
@@ -14,11 +14,25 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ColeccionEnvio coleccion = new ColeccionEnvio(10);
-        coleccion.AgregarParte(1, "Ace");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de articulos que desea agregar: ");
+        int num_partes = sc.nextInt();
+        ColeccionEnvio coleccion = new ColeccionEnvio(num_partes);
+        System.out.println("Buenos dias, ingrese que accion desea realizar: ");
+        do{
+            System.out.println("(1) Agregar unas Partes \n(2) Eliminar Partes \n
+        (3) Buscar una parte \n(4) Mostrar producto o parte \n(5) Mostrar toda la lista");
+        }while();
+        
+        int op = sc.nextInt();
+        switch(op){
+            
+        }
+        
+        /*coleccion.AgregarParte(1, "Ace");
         coleccion.AgregarParte(3, "Bimbo");
         coleccion.AgregarParte(2, "Alcachofa");
-        coleccion.ListaParte();
+        coleccion.ListaParte();*/
     }
     
 }
